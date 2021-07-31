3d41310258c88af33dad42e34d25b59691526f35
smartyjahid <66997363+smartyjahid@users.noreply.github.com>
2021-07-31 13:54:18 +0530

    Add files via upload

---
+++ request.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Cashfree - Signature Generator</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body onload="document.frm1.submit()">
+
+
+<?php 
+$mode = "TEST"; //<------------ Change to TEST for test server, PROD for production
+
+extract($_POST);
+  $secretKey = "805d837b2f2fae962330d75b0b834b1d2c4e196e";
+  $appId = "831790f2ba6e4fb6a37d0b40c97138";
+  $orderId = time();
+  $orderAmount = $orderAmount;
+  $returnUrl = "http://cfentergrition.myartsonline.com/response.php";
+  $orderCurrency = "INR";
+  $orderNote = "nhi chalega!";
+  $postData = array( 
+  "appId" => $appId, 
+  "orderId" => $orderId, 
+  "orderAmount" => $orderAmount, 
+  "orderCurrency" => $orderCurrency, 
+  "orderNote" => $orderNote, 
+  "customerName" => $customerName, 
+  "customerPhone" => $customerPhone, 
+  "customerEmail" => $customerEmail,
+  "returnUrl" => $returnUrl, 
+  // "notifyUrl" => $notifyUrl,
+);
+ksort($postData);
+$signatureData = "";
+foreach ($postData as $key => $value){
+    $signatureData .= $key.$value;
+}
+$signature = hash_hmac('sha256', $signatureData, $secretKey,true);
+$signature = base64_encode($signature);
+
+if ($mode == "PROD") {
+  $url = "https://www.cashfree.com/checkout/post/submit";
+} else {
+  $url = "https://test.cashfree.com/billpay/checkout/post/submit";
+}
+
+?>
+  <form action="<?php echo $url; ?>" name="frm1" method="post">
+      <p>Please wait.......</p>
+      <input type="hidden" name="signature" value='<?php echo $signature; ?>'/>
+      <input type="hidden" name="orderNote" value='<?php echo $orderNote; ?>'/>
+      <input type="hidden" name="orderCurrency" value='<?php echo $orderCurrency; ?>'/>
+      <input type="hidden" name="customerName" value='<?php echo $customerName; ?>'/>
+      <input type="hidden" name="customerEmail" value='<?php echo $customerEmail; ?>'/>
+      <input type="hidden" name="customerPhone" value='<?php echo $customerPhone; ?>'/>
+      <input type="hidden" name="orderAmount" value='<?php echo $orderAmount; ?>'/>
+      <!-- <input type ="hidden" name="notifyUrl" value='<?php echo $notifyUrl; ?>'/> -->
+      <input type ="hidden" name="returnUrl" value='<?php echo $returnUrl; ?>'/>
+      <input type="hidden" name="appId" value='<?php echo $appId; ?>'/>
+      <input type="hidden" name="orderId" value='<?php echo $orderId; ?>'/>
+  </form>
+</body>
+</html>
